b0c9f1d442f92337b11a783e98ae9cd5760fbd77
phongtiger <phongken94@gmail.com>
2019-11-01 22:11:06 +0700

    tailen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,34 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {Routes, RouterModule, PreloadAllModules} from '@angular/router';
+import {DictionaryPageComponent} from "./dictionary-page/dictionary-page.component";
+import {DictionaryComponent} from "./dictionary/dictionary.component";
+import {YoutubePlaylistComponent} from "./youtube-playlist/youtube-playlist.component";
+import {YoutubePlayerComponent} from "./youtube-player/youtube-player.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'dictionary',
+    component: DictionaryComponent
+  },
+  {
+    path: 'home',
+    component: DictionaryPageComponent
+  },
+  {
+    path: 'youtube',
+    component: YoutubePlaylistComponent,
+    children: [{
+      path: ':id',
+      component: YoutubePlayerComponent
+    }]
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,{
+    preloadingStrategy: PreloadAllModules
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DictionaryComponent } from './dictionary/dictionary.component';
+import { DictionaryPageComponent } from './dictionary-page/dictionary-page.component';
+import {YoutubePlaylistComponent} from "./youtube-playlist/youtube-playlist.component";
+import {YoutubePlayerComponent} from "./youtube-player/youtube-player.component";
 
+
+// @Injectable()
+// export class DictionaryService {}
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DictionaryComponent,
+    DictionaryPageComponent,
+    YoutubePlaylistComponent,
+    YoutubePlayerComponent
   ],
   imports: [
     BrowserModule,
